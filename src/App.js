ea9c09a851b22d5d8f20532b02fd665a8b37bb28
It21217654 <IT21217654@my.sliit.lk>
2023-12-24 23:14:45 +0530

    all

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Shop from './Pages/Shop';
@@ -7,24 +7,28 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/men_banner.jpg';
+import women_banner from './Components/Assets/women_banner.jpg';
+import kids_wear_banner from './Components/Assets/kids_wear_banner.jpg';
 
 function App() {
+
   return (
     <div>
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory cateory="mens"/>} />
-          <Route path="/womens" element={<ShopCategory cateory="womens"/>} />
-          <Route path="/kids" element={<ShopCategory cateory="kids"/>} />
-          <Route path="/product" element={<Product/>}>
-            <Route path=":productID" element={<Product/>}/>
+          <Route path="/mens" element={<ShopCategory banner={men_banner} category="mens" />} />
+          <Route path="/womens" element={<ShopCategory banner={women_banner} category="womens" />} />
+          <Route path="/kids" element={<ShopCategory banner={kids_wear_banner} category="Kids" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productID" element={<Product />} />
           </Route>
           <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<LoginSignup/>} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useContext } from 'react';
 import "./CSS/ShopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
+import dropdown from '../Components/Assets/dropdown.png';
+import Item from '../Components/Items/Item';
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+    const { all_products } = useContext(ShopContext);
+
+    const productCount = Array.isArray(all_products) ? all_products.length : 0;
+
     return (
-        <div className='Shop-category'>
+        <div>
+            {all_products && (
+                <div className='Shop-category'>
+                    <img src={props.banner} alt="" />
+                    <div className='shopcategory-indexSort'>
+                        <p>
+                            <span>showing 1-{productCount}</span> out of {productCount} products
+                        </p>
+                    </div>
+                    <div className='shopcategory-sort'>
+                        Sort by <img src={dropdown} alt="" />
+                    </div>
+                </div>
+            )}
+            {all_products && (
+                <div className='shopcategory-products'>
+                    {all_products.map((item, i) => {
+                        if (props.category === item.category) {
+                            return (
+                                <Item
+                                    key={i}
+                                    id={item.id}
+                                    name={item.name}
+                                    image={item.image}
+                                    new_price={item.new_price}
+                                    old_price={item.old_price}
+                                />
+                            );
+                        } else {
+                            return null;
+                        }
+                    })}
+                </div>
+            )}
         </div>
-    )
+    );
 }
-export default ShopCategory ;
+
+export default ShopCategory;
+
