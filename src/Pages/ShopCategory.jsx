bac816b694e4b0ca80fff574eb8c31a67b0151e7
It21217654 <IT21217654@my.sliit.lk>
2023-12-27 00:13:57 +0530

    initial commit

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,52 +1,67 @@
+
 import React, { useContext } from 'react';
 import "./CSS/ShopCategory.css";
 import { ShopContext } from "../Context/ShopContext";
 import dropdown from '../Components/Assets/dropdown.png';
 import Item from '../Components/Items/Item';
+import {
+  all_product_img1,
+  all_product_img2,
+  all_product_img3,
+  all_product_img4,
+  all_product_img5,
+  all_product_img6,
+  all_product_img7,
+  all_product_img8
+} from '../Components/Assets/all_product'; 
+const productData = [
+  all_product_img1,
+  all_product_img2,
+  all_product_img3,
+  all_product_img4,
+  all_product_img5,
+  all_product_img6,
+  all_product_img7,
+  all_product_img8
+];
 
 const ShopCategory = (props) => {
-    const { all_products } = useContext(ShopContext);
+  const { all_products } = useContext(ShopContext);
 
-    const productCount = Array.isArray(all_products) ? all_products.length : 0;
-
-    return (
-        <div>
-            {all_products && (
-                <div className='Shop-category'>
-                    <img src={props.banner} alt="" />
-                    <div className='shopcategory-indexSort'>
-                        <p>
-                            <span>showing 1-{productCount}</span> out of {productCount} products
-                        </p>
-                    </div>
-                    <div className='shopcategory-sort'>
-                        Sort by <img src={dropdown} alt="" />
-                    </div>
-                </div>
-            )}
-            {all_products && (
-                <div className='shopcategory-products'>
-                    {all_products.map((item, i) => {
-                        if (props.category === item.category) {
-                            return (
-                                <Item
-                                    key={i}
-                                    id={item.id}
-                                    name={item.name}
-                                    image={item.image}
-                                    new_price={item.new_price}
-                                    old_price={item.old_price}
-                                />
-                            );
-                        } else {
-                            return null;
-                        }
-                    })}
-                </div>
-            )}
+  return (
+    <div className='shop-category'>
+      <img src={props.banner} alt="" />
+      <div className="shopcategory-indexshort">
+        <p>
+          <span>Showing 1-8</span> out of 20 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown} alt="" />
         </div>
-    );
+      </div>
+      <div className='shopcategory-products'>
+        {all_products && all_products.map((item, i) => {
+          if (props.category === item.category) {
+            const productDetails = productData.find(product => product.id === item.id);
+
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={productDetails.name}
+                image={productDetails.image}
+                new_price={productDetails.new_price}
+                old_price={productDetails.old_price}
+              
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default ShopCategory;
-
